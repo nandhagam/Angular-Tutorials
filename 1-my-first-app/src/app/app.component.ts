084d00fc499b515f96c8e7ab2c22bd9194c13248
Nandhakumar <nandhagam@live.com>
2021-11-08 08:22:48 +0530

    LifeCycle Hooks and @Input

--- 1-my-first-app/src/app/app.component.ts
+++ 1-my-first-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,48 @@ import { Component } from '@angular/core';
   // template: '<div>{{title}}</div>',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
   title = 'my-first-app';
+
+  headerTitle = "Welcome Prithivi";
+
+  ngOnChanges() {
+    console.log("On Change Called")
+  }
+
+  ngAfterViewInit() {
+    console.log("After view init...")
+  }
+
+  ngAfterViewChecked() {
+    console.log("After View Checked")
+  }
+
+  ngOnInit() {
+    console.log("On Init Called.")
+    sessionStorage.setItem("prithivi", "Developer")
+  }
+
+  ngDoCheck() {
+    console.log("Do check Called.")
+  }
+
+  ngAfterContentInit() {
+    console.log("After Content Init..")
+  }
+
+  ngAfterContentChecked() {
+    console.log("After Content Checked")
+  }
+
+ 
+  ngOnDestroy() {
+    console.log("On Destroy Called.")
+    sessionStorage.removeItem("prithivi");
+  }
+
+
+  clickMe() {
+    this.title = "Prithivi's Angular Class"
+  }
 }
--- 1-my-first-app/src/app/header/header.component.ts
+++ 1-my-first-app/src/app/header/header.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Input, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnChanges, OnInit, DoCheck, OnDestroy, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+  
+  @Input('headerTitle') myHeaderTitle : string = 'Hello';
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges() {
+    console.log("On Change Called")
+  }
+
+  ngOnInit() {
+    console.log("On Init Called.")
+    sessionStorage.setItem("prithivi", "Developer")
+  }
+
+  ngDoCheck() {
+    console.log("Do check Called.")
+  }
+
+  ngAfterContentInit() {
+    console.log("After Content Init..")
+  }
+
+  ngAfterContentChecked() {
+    console.log("After Content Checked")
+  }
+
+  ngAfterViewInit() {
+    console.log("After view init...")
+  }
+
+  ngAfterViewChecked() {
+    console.log("After View Checked")
+  }
+
+
+  ngOnDestroy() {
+    console.log("On header Destroy Called.")
+    sessionStorage.removeItem("prithivi");
   }
 
 }
